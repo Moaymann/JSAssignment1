e65ed632e1bb6099e0153612fc47030d224204c0
Moaymann <leoayman98@gmail.com>
2023-02-25 16:43:27 +0200

    Js Ass 1

---
+++ JavaAss1.js
@@ -0,0 +1,248 @@
+
+   //Q1 var x=prompt ('enter a number')
+    //console.log(x) 
+    
+
+    //Q2
+    //var x=prompt('enter a number')
+    //if (x%3==0 && x%4==0){console.log('YES')}
+    //else {console.log('NO')}
+
+    //Q3
+    //var x=prompt('enter a number')
+    //var y=prompt('enter another number')
+    //if(x>y){console.log(x)}
+    //else {console.log(y)}
+
+    //Q4
+    //var x=prompt('enter a number')
+    //if (x<0){console.log('Negative')}
+    //else if(x>0){console.log('Positive')}
+
+    //Q5
+    // var a=prompt ('1-enter a number')
+    // var b=prompt ('2-enter a number')
+    // var c=prompt ('3-enter a number')
+    // if (a>b && b>c) 
+    //         {console.log(a)}
+    //     else if (b>a && b>c) 
+    //     {console.log(b)}
+            
+    //     else 
+    //     {console.log(c)} 
+    
+    
+    //     if (a<b && b<c) 
+    //     {console.log(a)}
+    //     else if (b<a && b<c) 
+    //     {console.log(b)}
+    //     else 
+    //     {console.log(c)}
+    
+    
+
+    // Q6
+
+//    var x=prompt ('enter a number')
+//    if (x%2==0){console.log('Even')}
+//    else {console.log('odd')}
+
+//Q8
+
+// var x=prompt('enter a word')
+// if (x=='a' || x=='e' || x=='i' || x=='o' || x=='u') {console.log('Noun')}
+// else {console.log('Consonant')}
+
+//Q9
+ //var x=prompt ('enter a number')
+ //for(i=0; i<=x; i++){console.log(i)}
+
+//Q10
+// var x=prompt ('enter a number')
+//  for(i=1; i<=12; i++){console.log(x*i)}
+
+//Q11
+// var x=prompt ('enter a number')
+//   for(i=1; i<=x; i++)
+//   if(i%2==0){console.log(i)}
+
+
+//  Q12
+//  var a=prompt ('1-enter a number')
+//  var b=prompt ('2-enter a number')
+//  {console.log(a**b)}
+
+// Q13
+//   var a=prompt ('1-enter a number')
+//   var b=prompt ('2-enter a number')
+//   var c=prompt ('3-enter a number')
+//   var d=prompt ('4-enter a number')
+//   var e=prompt ('5-enter a number')
+//   var x=(Number(a)+Number(b)+Number(c)+Number(d)+Number(e))
+//   var y=(x/5)
+//   var z=((x*100)/500)
+//  {console.log(x)}
+//  {console.log(y)}
+//  {console.log(z)}
+ 
+
+//  Q14
+//var x=prompt ('1-enter a number')
+
+// if(x==1){console.log('31')}
+// else if(x==2){console.log(28)}
+// else if(x==3){console.log(31)}
+// else if(x==4){console.log(30)}
+// else if(x==5){console.log(31)}
+// else if(x==6){console.log(30)}
+// else if(x==7){console.log(31)}
+// else if(x==8){console.log(31)}
+// else if(x==9){console.log(30)}
+// else if(x==10){console.log(31)}
+// else if(x==11){console.log(30)}
+// else if(x==12){console.log(31)}
+// else {console.log(' Homa 12 shahr bas ya raye2') }
+
+//Q15
+
+// var a=prompt ('1-Physics Mark')
+//    var b=prompt ('2-Chemistry Mark')
+//    var c=prompt ('3-Biology Mark')
+//   var d=prompt ('4-Mathematics Mark')
+//  var e=prompt ('5-Computer Mark')
+//  var x=(Number(a)+Number(b)+Number(c)+Number(d)+Number(e))
+//  var z=((x*100)/500)
+//  if(z>=90){console.log('Grade A')}
+//  else if(z>=80){console.log('Grade B')}
+//  else if(z>=70){console.log('Grade C')}
+//  else if(z>=60){console.log('Grade D')}
+//  else if(z>=40){console.log('Grade E')}
+//  else{console.log('Grade F')}
+
+
+//Q16
+
+// var x= prompt ('enter a month')
+// var days;
+
+// switch(x){
+// case '1': 
+//   case '3':
+//   case '5':
+//   case '7':
+//   case '8':
+//   case '10':
+//   case '12':
+//     days='31';
+    
+//     break;
+    
+//   case '4':
+//   case '6':
+//   case '9':
+//   case '11':
+//     days='30';
+    
+//     break;
+
+
+//   case '2':
+//     days='28';
+//     break;
+
+//     default: days= 'None';  
+// }
+// console.log(days);
+
+//Q17
+
+//  var x= prompt ('enter a letter')
+//  var letter;
+//  switch(x){
+//   case 'a':
+//   case 'e':
+//   case "i":
+//   case "o":
+//   case "u":
+//     letter='Noun';
+//     break;
+
+//     default:
+//       letter='Consonant';}
+
+//       console.log(letter);
+
+// Q18
+
+// var x= prompt ('enter a number')
+// var y= prompt ('enter another number')
+// switch(true){
+//   case (x > y):
+//     console.log(x);
+//     break;
+
+//     case (x< y):
+//     console.log(y);
+//     break;
+
+//     default:
+//       console.log('They are equal');
+// }
+
+
+ 
+//Q19
+
+// var x= prompt ('enter a number')
+//  switch(true){
+//   case (x%2==0):
+//   console.log('Even');
+//   break;
+
+//   default:
+//     console.log('Odd')
+//  }
+
+//Q20
+
+//  var x= prompt ('enter a number')
+//  switch(true){
+//   case (x>0):
+//     console.log('Number is positive');
+//     break;
+
+//     case (x<0):
+//     console.log('Number is negative');
+//     break;
+//     case (x==0):
+//       console.log('Number is zero');
+//       break;
+//       default:
+//          console.log('Unknown')
+//  }
+
+//Q21
+
+// var x=prompt('enter first number')
+// var y=prompt( 'enter operation')
+// var z=prompt('enter second number')
+// var result;
+// switch(y){
+//   case '+':
+//     result=Number(x)+Number(z)
+//     console.log(result);
+//     break;
+//     case '-':
+//     result=Number(x)-Number(z)
+//     console.log(result);
+//     break;
+//     case '*':
+//     result=Number(x)*Number(z)
+//     console.log(result);
+//     break;
+//     case '/':
+//     result=Number(x)/Number(z)
+//     console.log(result);
+//     break;
+// }
+
